d795de4daefa2514c3cdd21965b61f08e161292b
Henrique Ramos <henriqueramos0106@gmail.com>
2021-07-07 10:57:32 -0300

    Exibir os componentes de acordo com a etapa

--- src/components/FormularioCadastro/FormularioCadastro.tsx
+++ src/components/FormularioCadastro/FormularioCadastro.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DadosPessoais } from './DadosPessoais'
 import { DadosUsuario } from './DadosUsuario'
 import { DadosEntrega } from './DadosEntrega'
+import { Typography } from '@material-ui/core'
 
 export const FormularioCadastro = ({ aoEnviar, validarCPF }) => {
-  return (
-    <>
-      <DadosPessoais 
-        aoEnviar={aoEnviar} 
-        validarCPF={validarCPF}
-      />
+  const [etapaAtual, setEtapaAtual] = useState(1)
+
+  const formularioAtual = (etapa: number) => {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />
       
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+      case 1:
+        return <DadosPessoais 
+          aoEnviar={aoEnviar} 
+          validarCPF={validarCPF}
+        />
+      
+      case 2:
+        return <DadosEntrega />
+    
+      default: 
+      return <Typography>Erro ao selecionar formulário</Typography>
+    }
+  }
+  
+  return formularioAtual(etapaAtual)
 }
